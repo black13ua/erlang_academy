b081b081743a35fb57a53d058d791a232dec5511
black <v.cherednichenko@favbet.com>
2015-06-03 16:54:57 +0300

    Fix Warning in p04

--- lesson_01/p04.erl
+++ lesson_01/p04.erl
@@ -1,15 +1,9 @@
 -module(p04).
 -export([len/1]).
 
-%len([]) ->
-%	0;
-%len([H|T]) ->
-%	X = len(T),
-%	X + 1.
-
- len(List) ->
-   len(List,0).
- len([H|T],Acc) ->
-   len(T,Acc+1);
- len([],Acc) ->
+len(List) ->
+   len(List, 0).
+len([_H|T], Acc) ->
+   len(T, Acc+1);
+len([], Acc) ->
    Acc.
