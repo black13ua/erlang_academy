d8739e9dc02e3514db420869b4ea49b69326fc15
Black13.UA <black13.ua@gmail.com>
2015-06-04 08:59:37 -0400

    p07, Del Debug info in output

--- lesson_01/p07.erl
+++ lesson_01/p07.erl
@@ -7,12 +7,12 @@ flatten([], Acc) -> p05:reverse(Acc);
 
 flatten([[Hhead|Htail]|Tail], Acc) ->
    % Debug Info
-   io:fwrite("Clause 1 [Hhead, Htail, Tail]: ~p~n", [[Hhead, Htail, Tail]]),
-   io:fwrite("Clause 1 [Acc]: ~p~n", [Acc]),
+   %% io:fwrite("Clause 1 [Hhead, Htail, Tail]: ~p~n", [[Hhead, Htail, Tail]]),
+   %% io:fwrite("Clause 1 [Acc]: ~p~n", [Acc]),
    flatten(Htail ++ Tail, [Hhead|Acc]);
 
 flatten([H|T], Acc) ->
    % Debug Info
-   io:fwrite("Clause 2 [H, T]: ~p~n", [[H, T]]),
-   io:fwrite("Clause 2 [Acc]: ~p~n", [Acc]),
+   %% io:fwrite("Clause 2 [H, T]: ~p~n", [[H, T]]),
+   %% io:fwrite("Clause 2 [Acc]: ~p~n", [Acc]),
    flatten(T, [H|Acc]).
