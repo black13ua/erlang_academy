5a06b14b0b24787f49f2d4c5b46a67ede2807c91
Black13.UA <black13.ua@gmail.com>
2015-06-04 09:39:11 -0400

    p08, Some small fixes

--- lesson_01/p08.erl
+++ lesson_01/p08.erl
@@ -1,9 +1,14 @@
 -module(p08).
 -export([compress/1]).
 
-compress(L) -> compress(L,[]).
+compress(L) ->
+   compress(L,[]).
 
-compress([],Acc) -> p05:reverse(Acc);
-compress([H|T],[]) -> compress(T, [H]);
-compress([H|T],Acc=[H|_]) -> compress(T, Acc);
-compress([H|T],Acc) -> compress(T, [H|Acc]).
+compress([],Acc) ->
+   p05:reverse(Acc);
+compress([H|T],[]) ->
+   compress(T, [H]);
+compress([H|T],Acc=[H|_]) ->
+   compress(T, Acc);
+compress([H|T],Acc) ->
+   compress(T, [H|Acc]).
