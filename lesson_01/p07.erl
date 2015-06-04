2401b20272ccf3affa3452cad893aa0051f67ed8
Black13.UA <black13.ua@gmail.com>
2015-06-04 08:57:40 -0400

    Done p07, but not optimal solution!

--- lesson_01/p07.erl
+++ lesson_01/p07.erl
@@ -1,8 +1,18 @@
 -module(p07).
 -export([flatten/1]).
 
-flatten(L) -> flatten(L,[]).
+flatten(L) -> flatten(L, []).
 
-flatten([],Acc) -> Acc;
-flatten([[Hhead|Htail]|Tail],Acc) -> flatten(Htail, [Hhead|Acc]);
-flatten([H|T],Acc) -> flatten(T, [H|Acc]).
+flatten([], Acc) -> p05:reverse(Acc);
+
+flatten([[Hhead|Htail]|Tail], Acc) ->
+   % Debug Info
+   io:fwrite("Clause 1 [Hhead, Htail, Tail]: ~p~n", [[Hhead, Htail, Tail]]),
+   io:fwrite("Clause 1 [Acc]: ~p~n", [Acc]),
+   flatten(Htail ++ Tail, [Hhead|Acc]);
+
+flatten([H|T], Acc) ->
+   % Debug Info
+   io:fwrite("Clause 2 [H, T]: ~p~n", [[H, T]]),
+   io:fwrite("Clause 2 [Acc]: ~p~n", [Acc]),
+   flatten(T, [H|Acc]).
