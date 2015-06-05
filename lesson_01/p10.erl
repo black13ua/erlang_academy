c0668df4cbb40b189bf2eba35dc3a938a9e80759
Black13.UA <black13.ua@gmail.com>
2015-06-05 09:28:40 -0400

    p10 Done!

---
+++ lesson_01/p10.erl
@@ -0,0 +1,14 @@
+-module(p10).
+-export([encode/1, getfirst/1]).
+
+encode(List) ->
+   encode(p09:pack(List), []).
+
+encode([], Acc) ->
+   p05:reverse(Acc);
+
+encode([H|T], Acc) ->
+   encode(T, [{p04:len(H), getfirst(H)}|Acc]).
+
+getfirst([Head|_Tail]) ->
+   Head.
