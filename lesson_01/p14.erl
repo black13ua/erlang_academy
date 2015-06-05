f24b2cd843f1e36ca42d70dd8df12ade62dce7ed
Black13.UA <black13.ua@gmail.com>
2015-06-05 09:48:48 -0400

    p14 Done! It's so simple ...

--- lesson_01/p14.erl
+++ lesson_01/p14.erl
@@ -4,8 +4,7 @@
 duplicate(List) ->
    duplicate(List, []).
 
-duplicate([], Acc) ->
-   p05:reverse(Acc);
-
 duplicate([H|T], Acc) ->
-   duplicate(T, [H,H|Acc]).
+   duplicate(T, [H,H|Acc]);
+duplicate([], Acc) ->
+   p05:reverse(Acc).
