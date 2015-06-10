d15790486329cfec2f38906d72aba6df495e8368
Black13.UA <black13.ua@gmail.com>
2015-06-10 14:27:40 -0400

    bs03 - Some Idea ...

---
+++ lesson_02/bs03.erl
@@ -0,0 +1,48 @@
+-module(bs03).
+-export([split/2]).
+
+split(Binary, Delimeter) ->
+    parse_binary(Binary, list_to_binary([Delimeter]), len(Delimeter), <<>>, []).
+
+parse_binary(<<>>, _BinDltr, _DLen, BinAcc, Result) ->
+    Result;
+
+parse_binary(Binary, BinDltr, DLen, BinAcc = <<>>, Result) ->
+    <<BHead:DLen/binary, BTail/binary>> = Binary,
+    parse_binary(Binary, BinDltr, DLen, <<BHead/binary, BinAcc/binary>>, Result);
+
+parse_binary(Binary, BinDltr, DLen, BinAcc, Result) ->
+    <<BHead:DLen/binary, BTail/binary>> = Binary,
+    <<HBAcc:1/binary, TBAcc/binary>> = BinAcc,
+    case check_delimeter(BinDltr, BinAcc) of
+	true ->
+		good;
+	false ->
+		io:format("To loop: ~p~n", [[<<TBAcc/binary, BTail:1/binary>>]]),
+		parse_binary(<<TBAcc/binary, BTail/binary>>, BinDltr, DLen, <<TBAcc/binary, BTail:1/binary>>, <<HBAcc/binary>>)
+    end.
+
+
+check_delimeter(BinDltr, BinAcc) ->
+    %%% Debug output
+    io:format("Check Delimeter with: ~p~n", [[BinDltr, BinAcc]]),
+    %%%
+    if
+	BinDltr =:= BinAcc ->
+		true;
+		%io:format("Match Delimeter! ~p~n", [[BinDltr, BinAcc]]);
+	true ->
+		false
+    end.
+
+
+%%% Additional functions %%%
+%%%
+len(Delimeter) ->
+    len(Delimeter,0).
+ 
+len([], Acc) ->
+    Acc;
+len([_|T], Acc) ->
+    len(T, Acc+1).
+%%%
