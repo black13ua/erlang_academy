a154ad0378eab1eb9188fea3eab33a286936f807
Black13.UA <black13.ua@gmail.com>
2015-06-12 07:16:00 -0400

    bs03 - Full refactoring ...

--- lesson_02/bs03.erl
+++ lesson_02/bs03.erl
@@ -1,47 +1,17 @@
 -module(bs03).
 -export([split/2]).
 
-split(Binary, Delimeter) ->
-    parse_binary(Binary, list_to_binary([Delimeter]), len(Delimeter), <<>>, []).
+split(Bin, Delimeter) ->
+    DelBin = list_to_binary([Delimeter]),
+    DLen = byte_size(DelBin),
+    parse_bin(Bin, DelBin, DLen, <<>>, []).
 
-parse_binary(<<>>, _BinDltr, _DLen, BinAcc, Result) ->
-    Result;
-
-parse_binary(Binary, BinDltr, DLen, BinAcc = <<>>, Result) ->
-    <<BHead:DLen/binary, BTail/binary>> = Binary,
-    parse_binary(Binary, BinDltr, DLen, <<BHead/binary, BinAcc/binary>>, Result);
-
-parse_binary(Binary, BinDltr, DLen, BinAcc, Result) ->
-    <<BHead:DLen/binary, BTail/binary>> = Binary,
-    <<HBAcc:1/binary, TBAcc/binary>> = BinAcc,
-    case check_delimeter(BinDltr, BinAcc) of
-	true ->
-		parse_binary(BTail, BinDltr, DLen, <<>>, Result);
-	false ->
-		io:format("To loop: ~p~n", [[TBAcc, BTail, Result]]),
-		parse_binary(<<TBAcc/binary, BTail/binary>>, BinDltr, DLen, <<TBAcc/binary, BTail:1/binary>>, [HBAcc|Result])
+parse_bin(Bin, DelBin, DLen, WordAcc, Result) ->
+    case Bin of
+        <<DelBin:DLen/binary, Tail/binary>> ->
+	    parse_bin(<<Tail/binary>>, DelBin, DLen, <<>>, [WordAcc|Result]);
+        <<Byte:1/binary, Tail/binary>> ->
+	    parse_bin(<<Tail/binary>>, DelBin, DLen, <<WordAcc/binary, Byte/binary>>, Result);
+        <<>> ->
+	    lists:reverse([WordAcc|Result])
     end.
-
-check_delimeter(BinDltr, BinAcc) ->
-    %%% Debug output
-    io:format("Check Delimeter with: ~p~n", [[BinDltr, BinAcc]]),
-    %%%
-    if
-	BinDltr =:= BinAcc ->
-		true;
-		%io:format("Match Delimeter! ~p~n", [[BinDltr, BinAcc]]);
-	true ->
-		false
-    end.
-
-
-%%% Additional functions %%%
-%%%
-len(Delimeter) ->
-    len(Delimeter,0).
- 
-len([], Acc) ->
-    Acc;
-len([_|T], Acc) ->
-    len(T, Acc+1).
-%%%
