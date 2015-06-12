ee18ca8d05625bbd428d05dfbf6a8e57d26ece27
Black13.UA <black13.ua@gmail.com>
2015-06-12 06:07:32 -0400

    bs02 - refactoring ...

--- lesson_02/bs02.erl
+++ lesson_02/bs02.erl
@@ -1,18 +1,15 @@
 -module(bs02).
--export([words/1, parse_space/3]).
+-export([words/1]).
 
 words(Binary) ->
-   parse_space(Binary, <<>>, []).
+    parse_space(Binary, <<>>, []).
 
-parse_space(<<>>, Acc, Words) ->
-   lists:reverse([Acc|Words]);
-parse_space(<<Space:1/binary, Tail/binary>>, Acc, Words) ->
-   check_space(Space, Tail, Acc, Words).
-
-check_space(Space, Tail, Acc, Words) ->
-   if
-	Space =:= <<" ">> ->
+parse_space(Binary, Acc, Words) ->
+    case Binary of
+	<<" ", Tail/binary>> ->
 		parse_space(<<Tail/binary>>, <<>>, [Acc|Words]);
-	true ->
-		parse_space(<<Tail/binary>>, <<Acc/binary, Space/binary>>, Words)
-   end.
+	<<Letter:1/binary, Tail/binary>> ->
+		parse_space(<<Tail/binary>>, <<Acc/binary, Letter/binary>>, Words);
+	<<>> ->
+		lists:reverse([Acc|Words])
+    end.
